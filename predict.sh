2d6c0cc3507eb0709bf47882c272ac359ca25cce
Chi-Hung Chang <chihung861224@gmail.com>
2022-01-25 00:16:05 +0800

    script for predicting all datasets

--- predict.sh
+++ predict.sh
@@ -1,6 +1,14 @@
-echo "struc2vec"
-python3 predict.py --dataset Cora --embed emb/Cora-struc2vec.emb
-echo "HPE"
-python3 predict.py --dataset Cora --embed emb/Cora-HPE.emb
-echo "struc2vec+HPE"
-python3 predict.py --dataset Cora --embed emb/Cora-struc2vec-HPE.emb
+for dataset in "Cora" "CiteSeer" "facebook348" "brazil-airports" "europe-airports" "usa-airports"
+do
+    echo "--------------------"
+    echo $dataset
+    echo 
+    echo ">> struc2vec"
+    python3 predict.py --dataset $dataset --embed emb/$dataset-struc2vec.emb
+    echo
+    echo ">> HPE"
+    python3 predict.py --dataset $dataset --embed emb/$dataset-HPE.emb
+    echo
+    echo ">> struc2vec+HPE"
+    python3 predict.py --dataset $dataset --embed emb/$dataset-struc2vec-HPE.emb
+done
