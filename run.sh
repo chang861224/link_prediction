645ea042b3b7ca9b82725dc1a371320320d3adc6
Chi-Hung Chang <chihung861224@gmail.com>
2022-01-29 00:54:24 +0800

    add process of generating weighted edgelist

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
-python3 split.py --dataset Cora
-python3 struc2vec/src/main.py --input split/train_Cora.edgelist --output emb/Cora-struc2vec.emb
-./smore/cli/hpe -train split/train_Cora_weighted.edgelist -save emb/Cora-HPE.emb -dimensions 128 -undirected 1 -sample_times 1200 -walk_steps 5 -threads 8
-./smore/cli/hpe -train split/train_Cora_weighted.edgelist -save emb/Cora-struc2vec-HPE.emb -load_v emb/Cora-struc2vec.emb -dimensions 128 -undirected 1 -sample_times 1200 -walk_steps 5 -threads 8
+for dataset in "Cora" "CiteSeer" "facebook348" "brazil-airports" "europe-airports" "usa-airports"
+do
+    #python3 split.py --dataset $dataset
+    #python3 struc2vec/src/main.py --input split/train_$dataset.edgelist --output emb/$dataset-struc2vec.emb
+    python3 genNetwork.py --dataset $dataset --weighted
+    ./smore/cli/hpe -train split/train_$dataset\_weighted.edgelist -save emb/$dataset-HPE.emb -dimensions 128 -undirected 1 -sample_times 2000 -walk_steps 5 -threads 8
+    ./smore/cli/hpe -train split/train_$dataset\_weighted.edgelist -save emb/$dataset-struc2vec-HPE.emb -load_v emb/$dataset-struc2vec.emb -dimensions 128 -undirected 1 -sample_times 2000 -walk_steps 5 -threads 8
+done
