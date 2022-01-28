b016b2144b9c16913111add640c1f3ca285bc577
Chi-Hung Chang <chihung861224@gmail.com>
2022-01-29 00:56:30 +0800

    modify HPE training "sample_times" to 1200

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ do
     #python3 split.py --dataset $dataset
     #python3 struc2vec/src/main.py --input split/train_$dataset.edgelist --output emb/$dataset-struc2vec.emb
     python3 genNetwork.py --dataset $dataset --weighted
-    ./smore/cli/hpe -train split/train_$dataset\_weighted.edgelist -save emb/$dataset-HPE.emb -dimensions 128 -undirected 1 -sample_times 2000 -walk_steps 5 -threads 8
-    ./smore/cli/hpe -train split/train_$dataset\_weighted.edgelist -save emb/$dataset-struc2vec-HPE.emb -load_v emb/$dataset-struc2vec.emb -dimensions 128 -undirected 1 -sample_times 2000 -walk_steps 5 -threads 8
+    ./smore/cli/hpe -train split/train_$dataset\_weighted.edgelist -save emb/$dataset-HPE.emb -dimensions 128 -undirected 1 -sample_times 1200 -walk_steps 5 -threads 8
+    ./smore/cli/hpe -train split/train_$dataset\_weighted.edgelist -save emb/$dataset-struc2vec-HPE.emb -load_v emb/$dataset-struc2vec.emb -dimensions 128 -undirected 1 -sample_times 1200 -walk_steps 5 -threads 8
 done
